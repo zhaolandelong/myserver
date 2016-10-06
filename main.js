edbbc189f56b3d135d1160d774cc4a4a8f8d8ea6
zhaolandelong <zhaolandelong@163.com>
2016-10-06 16:26:47 +0800

    first push

---
+++ main.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    io = require('socket.io'),
+    app = express();
+app.use(express.static(__dirname));
+var server = app.listen(8888),
+    ws = io.listen(server);
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+ws.on('connection', function(client) {
+    console.log('someone is connect\n');
+    client.on('join', function(msg) {
+        //check overlap
+        if (checkNickname(msg)) {
+            client.emit('nickname', 'nickname is overlapped!');
+        } else {
+            client.nickname = msg;
+            ws.sockets.emit('announcement', 'system', msg + ' joined');
+        }
+    });
+    //listen send
+    client.on('send.message', function(msg) {
+        client.broadcast.emit('send.message', client.nickname, msg);
+    });
+    //when disconnect info others
+    client.on('disconnect', function() {
+        if (client.nickname) {
+            client.broadcast.emit('send.message', 'system', client.nickname + ' leaved!');
+        }
+    });
+});
+//check nickname overlap
+var checkNickname = function(name) {
+    var k, sk = ws.sockets.sockets;
+    for (var k in sk) {
+        if (sk.hasOwnProperty(k)) {
+            if (sk[k] && sk[k].nickname == name) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
