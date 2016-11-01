7c27390a63cadb6622d8a2479a19e77111d4ad16
zhaolandelong <zhaolandelong@163.com>
2016-11-01 23:51:47 +0800

    start change folders

---
+++ config/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+module.exports = function() {
+    console.log('init express...');
+    var app = express();
+
+    app.use(bodyParser.json());
+
+    app.use(function(req, res, next) {
+        res.status(404);
+        try {
+            return res.json('Not Found');
+        } catch (e) {
+            console.error('404 set header after sent');
+        }
+    });
+
+    app.use(function(err, req, res, next) {
+        if (!err) {
+            return next();
+        }
+        res.status(500);
+        try {
+            return res.json(err.message || 'server error');
+        } catch (e) {
+            console.error('500 set header after sent');
+        }
+    });
+
+    return app;
+};
