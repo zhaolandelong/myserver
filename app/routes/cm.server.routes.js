2e2ceb7bcb28160ae0fc69d69afa17729d7c31e8
zhaolandelong <zhaolandelong@163.com>
2016-11-06 23:10:20 +0800

    add getOne api and change index.html

--- app/routes/cm.server.routes.js
+++ app/routes/cm.server.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const CMctr = require('../controllers/cm.server.controller.js');
 
 router.get('/getQues', CMctr.getQues);
+router.get('/getOne', CMctr.getOne);
 router.post('/postQues', CMctr.postQues);
 router.post('/postAns', CMctr.postAns);
 router.post('/delQues', CMctr.delQues);
