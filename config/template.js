ede3fedebe60f0e587ac9ec2e6952808d7459955
zhaolandelong <zldl@localhost.localdomain>
2016-11-14 12:37:54 +0800

    sv

--- config/template.js
+++ config/template.js
@@ -1,6 +1,6 @@
 const template = require('art-template');
 // template.config('base','');
 template.config('extname', '.html');
-template.helper('api', (data) => JSON.stringify(data, null, 2));
+template.helper('api', (data) => JSON.stringify(data, null, 4));
 template.helper('dollar', (data) => '$' + data);
 module.exports = template;
